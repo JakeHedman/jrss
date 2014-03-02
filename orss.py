ca01003724fea9d5f78f2a8131cc755a5c767311
ooskr <ooskar@ooskar.com>
2014-03-02 18:47:47 +0100

    Added user-agents, some trackers needs this.
    Also supports use of multiple trackers now. Added includes (for e.g. 720p) and excludes for languages you dont want or something like that.

--- orss.py
+++ orss.py
@@ -5,56 +5,81 @@ from os import listdir
 # Shows to autodownload
 shows=[
 	'Anger.Management',
-	'Arrested.Development',
-	'Awkward',
-	'Blue.Mountain.State',
+	'Bates.Motel',
+	'Betas',
+        'Better.Call.Saul',
 	'Californication',
 	'Community',
-	'Cougar.Town',
-	'Dexter',
-	'Happy.Endings',
+        'Family.Guy',
+	'Graceland',
+	'Homeland',
+        'House.of.Lies',
 	'How.I.Met.Your.Mother',
-	'Melissa.And.Joey',
+	'Legit',
+	'Men.at.Work',
 	'Modern.Family',
+	'MythBusters',
 	'New.Girl',
 	'Punkd',
+	'Ray.Donovan',
 	'Revolution',
 	'Royal.Pains',
 	'Shameless.US',
-	'Suburgatory',
 	'Suits',
 	'The.Big.Bang.Theory',
-	'The.Office.US',
+	'The.Crazy.Ones',
 	'The.Following',
+	'The.Goldbergs',
+        'The.Simpsons',
 	'Top.Gear',
 	'Top_Gear',
-	'Two.And.A.Half.Men',
+	'Two.and.a.Half.Men',
+	'Under.the.Dome',
 	'Wilfred',
-	'Workaholics'
+	'Workaholics',
 ]
 
 
 # The filter may match some unwanted shows, double check first
 excludes=[
 	'Top.Gear.US',
+	'DUBBED',
+	'dubbed',
+	'Dubbed',
+	'GERMAN',
+	'German',
+	'german',
+]
+
+includes=[
+	'720p',
 ]
 
 # Link to the rss to use
-rsslink = "https://domain.tld/rss"
+rsslinks=[
+        'https://tracker.com/blablabla',
+]
 
 # Path where to save .torrent files
-torrentpath = "/Users/username/torrents/" # End with a slash
+torrentpath = "/Users/username/Torrents/" # End with a slash
 
 
-f = feedparser.parse(rsslink)
+feedparser.USER_AGENT = "uTorrent 1.6.1.0 uTorrent/1610"
 
-for item in f['entries']:
-	for show in shows:
-		if show in item['title']:
-			for exclude in excludes:
-				if exclude not in item['title']:
-					if item['title'] + ".torrent" not in listdir(torrentpath):
-						torrent = urllib2.urlopen(item['link']).read()
-						file = open(torrentpath + item['title'] + ".torrent","w+")
-						file.write(torrent)
-						file.close()
+#f = feedparser.parse(rsslink)
+for rss in rsslinks:
+	f = feedparser.parse(rss)
+	for item in f['entries']:
+		for show in shows:
+			if show in item['title']:
+				for include in includes:
+					if include in item['title']:
+						for exclude in excludes:
+							if exclude not in item['title']:
+								if item['title'] + ".torrent" not in listdir(torrentpath):
+                                                                        headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36' }
+                                                                        request = urllib2.Request(item['link'], None, headers)
+									torrent = urllib2.urlopen(request).read()
+									file = open(torrentpath + item['title'] + ".torrent","w+")
+									file.write(torrent)
+									file.close()
