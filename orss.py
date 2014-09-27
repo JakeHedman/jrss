c641ab1d5081a61fb733ec2875e0544346a36a95
Jakob Hedman <jakob@hedman.email>
2014-09-27 14:35:42 +0200

    Remove nestled loops Fix exclude

--- orss.py
+++ orss.py
@@ -4,55 +4,51 @@ from os import listdir
 
 # Shows to autodownload
 shows=[
-	'Anger.Management',
-	'Bates.Motel',
-	'Betas',
-        'Better.Call.Saul',
-	'Californication',
-	'Community',
-        'Family.Guy',
-	'Graceland',
-	'Homeland',
-        'House.of.Lies',
-	'How.I.Met.Your.Mother',
-	'Legit',
-	'Men.at.Work',
-	'Modern.Family',
-	'MythBusters',
-	'New.Girl',
-	'Punkd',
-	'Ray.Donovan',
-	'Revolution',
-	'Royal.Pains',
-	'Shameless.US',
-	'Suits',
-	'The.Big.Bang.Theory',
-	'The.Crazy.Ones',
-	'The.Following',
-	'The.Goldbergs',
-        'The.Simpsons',
-	'Top.Gear',
-	'Top_Gear',
-	'Two.and.a.Half.Men',
-	'Under.the.Dome',
-	'Wilfred',
-	'Workaholics',
+    'anger.management',
+    'bates.motel',
+    'betas',
+        'better.call.saul',
+    'californication',
+    'community',
+        'family.guy',
+    'graceland',
+    'homeland',
+        'house.of.lies',
+    'how.i.met.your.mother',
+    'legit',
+    'men.at.work',
+    'modern.family',
+    'mythbusters',
+    'new.girl',
+    'punkd',
+    'ray.donovan',
+    'revolution',
+    'royal.pains',
+    'shameless.us',
+    'suits',
+    'the.big.bang.theory',
+    'the.crazy.ones',
+    'the.following',
+    'the.goldbergs',
+        'the.simpsons',
+    'top.gear',
+    'top_gear',
+    'two.and.a.half.men',
+    'under.the.dome',
+    'wilfred',
+    'workaholics',
 ]
 
 
 # The filter may match some unwanted shows, double check first
 excludes=[
-	'Top.Gear.US',
-	'DUBBED',
-	'dubbed',
-	'Dubbed',
-	'GERMAN',
-	'German',
-	'german',
+    'top.gear.us',
+    'dubbed',
+    'german',
 ]
 
 includes=[
-	'720p',
+    '720p',
 ]
 
 # Link to the rss to use
@@ -66,20 +62,35 @@ torrentpath = "/Users/username/Torrents/" # End with a slash
 
 feedparser.USER_AGENT = "uTorrent 1.6.1.0 uTorrent/1610"
 
-#f = feedparser.parse(rsslink)
+def download(item):
+    # Remove items not in shows
+    for show in shows:
+        if show in item['title'].lower():
+            break
+    else:
+        return
+    
+    # Remove items not in includes
+    for include in includes:
+        if not include in item['title'].lower():
+            return
+
+    # Remove items in excludes
+    for exclude in excludes:
+        if exclude in item['title'].lower():
+            return
+
+    if item['title'] + ".torrent" in listdir(torrentpath):
+        return
+
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36' }
+    request = urllib2.Request(item['link'], None, headers)
+    torrent = urllib2.urlopen(request).read()
+    file = open(torrentpath + item['title'] + ".torrent", "w+")
+    file.write(torrent)
+    file.close()
+
 for rss in rsslinks:
-	f = feedparser.parse(rss)
-	for item in f['entries']:
-		for show in shows:
-			if show in item['title']:
-				for include in includes:
-					if include in item['title']:
-						for exclude in excludes:
-							if exclude not in item['title']:
-								if item['title'] + ".torrent" not in listdir(torrentpath):
-                                                                        headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36' }
-                                                                        request = urllib2.Request(item['link'], None, headers)
-									torrent = urllib2.urlopen(request).read()
-									file = open(torrentpath + item['title'] + ".torrent","w+")
-									file.write(torrent)
-									file.close()
+    f = feedparser.parse(rss)
+    for item in f['entries']:
+        download(item)
